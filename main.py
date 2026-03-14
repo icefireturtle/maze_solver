fa5fe1e0843f0ad027407eb66f29d57e6558ce2f
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-03-13 21:49:19 -0500

    first commit includes building tkinter window in a window class, setting up the constructor, adding 3 methods then built main calls

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("Tkinter Window")
+        self.__canvas = Canvas(self.__root, width=self.width, height=self.height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running_state = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running_state = True
+        while self.__running_state == True:
+            self.redraw()
+        
+    def close(self):
+        self.__running_state = False
+        
+
+def main():
+    window = Window(800, 600)
+    window.wait_for_close()
+
+main()
